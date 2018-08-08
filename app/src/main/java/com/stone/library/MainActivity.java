2b7e99f96e1929d9092ec9a2ad538c2d46d9e79e
朱磊 <zhulei199010@yahoo.com>
2018-08-08 23:27:36 +0800

    remove log and add demo

--- app/src/main/java/com/stone/library/MainActivity.java
+++ app/src/main/java/com/stone/library/MainActivity.java
@@ -1,7 +1,11 @@
 package com.stone.library;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.stone.keyboardview.model.Key;
+import com.stone.keyboardview.widget.KeyboardView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
     }
+
+    private void initView() {
+        KeyboardView kv = findViewById(R.id.keyboard);
+        kv.setOnKeyClickListener(new KeyboardView.OnKeyClickListener() {
+            @Override
+            public void onKeyClicked(String key, Key.Action action) {
+                Toast.makeText(MainActivity.this, key, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
